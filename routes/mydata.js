0cd8d0954496c379f8644639ed87f8f3ebc6145f
SiddharthaSangani <156238859+SiddharthaSangani@users.noreply.github.com>
2024-03-18 14:50:17 -0500

    finalcomit

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Siddhartha Sangani' });
+  res.render('mydata', { title: 'Siddhartha Sangani' });
 });
 
 module.exports = router;
