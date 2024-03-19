ec904ba1b40e3432c17be05147b1aaac701dcc05
SiddharthaSangani <156238859+SiddharthaSangani@users.noreply.github.com>
2024-03-19 01:20:53 -0500

    endcommit

--- routes/computation.js
+++ routes/computation.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var x = Math.random()*(10-(-1))+(-1);
-/* GET mydata page. */
+/* GET compute page. */
 router.get('/', function(req, res, next) {
    var y = Math.tanh(x);
   res.send(`tanh[${x}] is [${y}]`);
