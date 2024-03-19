9501501f8538ecb392dfbefae2d5702b45612dfb
SiddharthaSangani <156238859+SiddharthaSangani@users.noreply.github.com>
2024-03-19 01:16:10 -0500

    last

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
-var computeRouter = require('./routes/compute');
+var computeRouter = require('./routes/computation');
 
 var app = express();
 
