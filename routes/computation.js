f622cbcf48b42b6fc0a70eb987de884178b93244
SiddharthaSangani <156238859+SiddharthaSangani@users.noreply.github.com>
2024-03-19 01:19:05 -0500

    ls

--- routes/computation.js
+++ routes/computation.js
@@ -4,7 +4,7 @@ var x = Math.random()*(10-(-1))+(-1);
 /* GET mydata page. */
 router.get('/', function(req, res, next) {
    var y = Math.tanh(x);
-  res.send(`f[${x}] is [${y}]`);
+  res.send(`tanh[${x}] is [${y}]`);
 });
 
 module.exports = router;
