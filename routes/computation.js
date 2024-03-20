e5f91afc5ff71c3426b5245306e1daf63c2d05b1
SiddharthaSangani <156238859+SiddharthaSangani@users.noreply.github.com>
2024-03-20 16:23:00 -0500

    finbonus

--- routes/computation.js
+++ routes/computation.js
@@ -3,8 +3,16 @@ var router = express.Router();
 var x = Math.random()*(10-(-1))+(-1);
 /* GET compute page. */
 router.get('/', function(req, res, next) {
-   var y = Math.tanh(x);
-  res.send(`tanh[${x}] is [${y}]`);
+  var y = Math.tanh(x);
+  var z = Math.trunc(x); 
+  var a = Math.sign(x);  
+  var b = Math.pow(x,x);
+  res.render("computation",{
+    y: `tanh [${x}] is ${y}`,
+    z: `trunc [${x}] is ${z}`,
+    a: `sign [${x}] is ${a}`,
+    b: ` ${x} to ${x} is ${b}`
+})
 });
 
 module.exports = router;
